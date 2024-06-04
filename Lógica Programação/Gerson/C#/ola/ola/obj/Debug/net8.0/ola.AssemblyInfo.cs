14105cb06325cf917fbe7f1a873bcadedcda3f9f
luquinhaixx <lucas.moserdasilva@gmail.com>
2024-06-04 15:42:17 -0300

    opa

--- Lógica Programação/Gerson/C#/ola/ola/obj/Debug/net8.0/ola.AssemblyInfo.cs
+++ Lógica Programação/Gerson/C#/ola/ola/obj/Debug/net8.0/ola.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("example")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c4b470c6b0bce4c1b79e5afbdd12424b11129b")]
-[assembly: System.Reflection.AssemblyProductAttribute("example")]
-[assembly: System.Reflection.AssemblyTitleAttribute("example")]
+[assembly: System.Reflection.AssemblyProductAttribute("ola")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
