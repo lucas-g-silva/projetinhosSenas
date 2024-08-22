23407aca127405e448f05de2ea7e9f08d8e5d0d3
Lucas Gabriel <152902075+lucas-g-silva@users.noreply.github.com>
2024-08-22 16:54:37 -0300

    Aulinha de HTML e CSS do Gersu

---
+++ AI PSII 2024/Desenvolvimento de Sistemas/Gerson/AppForm/AppCadastro/src/AppCad.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class AppCad extends JFrame {
+
+    public AppCad(){
+        setTitle("Teste de Aplicação Desktop");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+    }
+    public static void main(String[] args) throws Exception {
+        AppCad form = new AppCad();
+    }
+}
